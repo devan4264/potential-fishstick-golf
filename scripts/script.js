b650015c00676da62f5d79f11d9d23ba93bf540b
Devan Bush <157516863+devan4264@users.noreply.github.com>
2024-03-15 23:49:32 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,40 @@
-import { gameData } from "./GameData";
+import { GameData } from "./GameData.js";
 
-const gamesContainer = GameData.getElementById('gamesContainer');
+$(function () {
+  // iterate over GameData array without knowing how many objects in the DOM
 
-// Create 29 cards
-for (let i = 1; i <= 29; i++) {
-  const card = GameData.createElement('div');
-  card.classList.add('card');
-  card.innerHTML = `
-    <h2>Name ${i}</h2>
-    <p>Description</p>
-    <a href="#">Link</a>
-  `;
-  gamesContainer.appendChild(card);
-}
+  $.each(GameData, function (index, item) {
+    
+    // make cards "cooking"
+    var elem = `<div class='card text-center mx-auto bg-dark blue' style="width: 200px"> \
+  <button class='cardOpen btn btn-outline-info'> ${item.gameName} </button> \
+   <div class='cardHide'> \
+       <br> \
+       <a href="${item.gameURL}" target="_blank" class="link-info" >Game URL</a> \
+       <p class="text-white">Author: ${item.gameAuthor} </p> \
+       <br> \
+       <button class='btn btn-outline-info cardClose'>Close</button> \
+   </div>
+ </div>`;
+
+    $("#cards").append(elem);
+  });
+});
+
+
+// cool card script: adopt/orphan
+$(function () {
+    $(".cardClose").click(function () {
+      $(this).parent().hide();
+    });  
+  
+    $(".cardOpen").click(function () {
+      $(this).parent().children().show();
+    });
+  });
+   
+    // toggle adopt/orphan
+    $("#nav-toggle").click(function () {
+      $("nav ul").slideToggle();
+    });
+    
